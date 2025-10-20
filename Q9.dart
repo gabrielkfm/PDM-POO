e20b2dda96400d344bc5061525e75850a7e81023
gabrielkfm <sousa.gabriel08@aluno.ifce.edu.br>
2025-10-19 23:29:46 -0300

    Update Q9.dart
    Added a Zoologico class with a list of animals and a method to traverse cages.

--- Q9.dart
+++ Q9.dart
@@ -1,6 +1,25 @@
-class Veterinario {
-  void examinar(Animal animal) {
-    print('Examinando ${animal.nome}...');
-    animal.emitirSom();
+class Zoologico {
+  List<Animal> jaulas = [];
+
+  Zoologico() {
+    jaulas = [
+      Cachorro('Rex'),
+      Cavalo('Pé de Pano'),
+      Gato('Mimi'),
+      Cachorro('Bob'),
+      Gato('Luna'),
+      Cavalo('Tornado'),
+      Cachorro('Spike'),
+      Gato('Garfield'),
+      Cavalo('Spirit'),
+      Cachorro('Bolt'),
+    ];
+  }
+
+  void percorrerJaulas() {
+    for (var animal in jaulas) {
+      animal.emitirSom();
+      animal.correr();
+    }
   }
 }
